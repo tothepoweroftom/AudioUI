af50f082d1b0c433f2ea222a24a8f3cef8cc88d0
Tom Power <tom.power@mobgen.com>
2017-02-09 14:26:29 +0100

    Initial Commit

---
+++ Swix_DSP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swix_DSP
+//
+//  Created by Tom Power on 09/02/2017.
+//  Copyright © 2017 MOBGEN:Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
